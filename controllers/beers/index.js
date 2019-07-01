8f636da7f05e0b4772125502316c5e9cf1dee2e3
Alex Aguilar <auza920205@gmail.com>
2019-07-01 13:39:36 -0500

    beers controller let to var

--- controllers/beers/index.js
+++ controllers/beers/index.js
@@ -3,19 +3,19 @@ var Product = require('../../models/Product.js');
 
 module.exports = function(router) {
 
-  router.get('/', async(req, res) => {
+  router.get('/', async function(req, res) {
     var product = new Product();
     product = await product.readProducts();
     res.send(product);
   });
 	
-	router.post('/', async(req, res) => {
+	router.post('/', async function(req, res) {
     var product = new Product(req.body.name, req.body.quantity);
 		product = await product.createProduct();
 		res.send(product);
   });
   
-  router.put('/' , async(req, res) => {
+  router.put('/' , async function(req, res) {
     var product = new Product(req.body.name, req.body.quantity);
     product = await product.updateProduct(req.body.id);
     res.send(product);
